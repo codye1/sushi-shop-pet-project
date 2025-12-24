88af7617d255a07cf883cd947d23ef2b8a8fc759
codye1 <123339584+codye1@users.noreply.github.com>
2025-12-25 00:35:42 +0200

    change cors

--- server/index.js
+++ server/index.js
@@ -13,11 +13,20 @@ console.log(process.env.CORS);
 const PORT = process.env.PORT || 5000
 
 server.use(cors({
-  //https://sushi-shop-pet-project.vercel.app
-  //http://localhost:5173
-  origin: process.env.CORS,
+  origin: function (origin, callback) {
+    // 1. Allow if origin is empty (e.g., mobile apps)
+    // 2. Allow if it matches the main CORS variable
+    // 3. Allow if it is any of your branches on Vercel
+    if (!origin ||
+        origin === process.env.CORS ||
+        origin.includes("sushi-shop-pet-project") && origin.endsWith(".vercel.app")) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
-}))
+}));
 
 server.options('*', cors())
 
